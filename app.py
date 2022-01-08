7a063cdffc34de89b9496d8db11c6632224c6361
MoshaLangerak <82883699+MoshaLangerak@users.noreply.github.com>
2022-01-08 08:20:06 +0100

    Functioning map visualisation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from jbi100_app.main import app
 from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
-from jbi100_app.data import get_data
+from jbi100_app.data import create_districts_df, merge_df, stats_per_capita, load_accident_data, load_population_data, load_geojson_data
 
 from dash import html
 from dash import dcc
@@ -12,57 +12,57 @@ import plotly.graph_objects as go
 
 if __name__ == '__main__':
     # Create data
-    df = px.data.iris()
-    df_complete = get_data()
-    df_accidents = df_complete.loc['accidents']
+    file_path_1 = 'all_years_2000_2020.csv'
+    file_path_2 = 'Population data 2018.csv'
+    file_path_geojson = 'uk_la.geojson'
+
+    stats = ['Accident Severity', 'Number of vehicles', 'Number of casualties']
+    token = 'pk.eyJ1IjoibW9zaGEtbGFuZ2VyYWsiLCJhIjoiY2t3ZGoxMmtxMGt2ZzJudDN5NXA0YWQ4ciJ9.6QIBuYFeAajMhniiNVU-GA'
+    statistics = ['Accident Severity', 'Number of vehicles', 'Number of casualties', 'Accident Severity Per capita', 'Number of vehicles Per capita', 'Number of casualties Per capita']
+
+    df_pd = load_accident_data(file_path_1)
+    df_pop = load_population_data(file_path_2)
+    df_grouped = create_districts_df(df_pd)
+    df_districts = merge_df(df_grouped, df_pop)
+    stats_per_capita(df_districts, stats)
+    geojson = load_geojson_data(file_path_geojson)
 
     # Instantiate custom views
-    scatterplot1 = Scatterplot("Scatterplot 1", 'sepal_length', 'sepal_width', df)
+    scatterplot1 = Scatterplot("Scatterplot jhgfh", 'sepal_length', 'sepal_width', df)
     scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
     fig = go.Figure(go.Densitymapbox(lat=df_accidents['latitude'], lon=df_accidents['longitude'], z=df_accidents['accident_severity'], radius=10))
 
     app.layout = html.Div(
-        id="app-container",
-        children=[
-            # Left column
-            html.Div(
-                id="left-column",
-                className="three columns",
-                children=make_menu_layout()
-            ),
-
-            # Right column
-            html.Div(
-                id="right-column",
-                className="nine columns",
-                children=[
-                    scatterplot1,
-                    scatterplot2, 
-                    dcc.Graph(
-                        id='map-graph',
-                        figure=fig
-                    )
-                ],
-            ),
-        ],
+        # dropdown
+        html.P([
+            html.Label("Choose a feature"),
+            dcc.Dropdown(id = 'stat', 
+                            options = [{'value': x, 'label': x} for x in statistics],
+                            value = statistics[0])
+                ], style = {'width': '400px',
+                            'fontSize' : '20px',
+                            'display': 'inline-block'}),
+        
+        dcc.Graph(id="choropleth"),
     )
 
-    # Define interactions
     @app.callback(
-        Output(scatterplot1.html_id, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        Input(scatterplot2.html_id, 'selectedData')
-    ])
-    def update_scatter_1(selected_color, selected_data):
-        return scatterplot1.update(selected_color, selected_data)
+        Output("choropleth", "figure"), 
+        [Input("stat", "value")])
 
-    @app.callback(
-        Output(scatterplot2.html_id, "figure"), [
-        Input("select-color-scatter-2", "value"),
-        Input(scatterplot1.html_id, 'selectedData')
-    ])
-    def update_scatter_2(selected_color, selected_data):
-        return scatterplot2.update(selected_color, selected_data)
+    def display_choropleth(stat):
+        fig = px.choropleth_mapbox(
+            df_districts, geojson=geojson, color=stat,
+            locations="Local Authority District Code", featureidkey= 'properties.geo_code',
+            mapbox_style='dark')
+        fig.update_layout(margin={'r':0, 't':0, 'l':0, 'b':0},
+            mapbox_zoom=4.3, # use this to zoom in on the map
+            mapbox_center_lat = 54.5, # use this to align the map on latitude
+            mapbox_center_lon = -3, # use this to align the map on longitude
+            mapbox_accesstoken=token, # token used for getting a different map type
+            width=800,
+            height=750)
+        return fig
 
-
+    
     app.run_server(debug=False, dev_tools_ui=True)
--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,19 +1,39 @@
 import plotly.express as px
 import pandas as pd
+import json
 
+def create_districts_df(df : pd.DataFrame) -> pd.DataFrame:
+    df_grouped = df.groupby('local_authority_ons_district').sum().reset_index()
+    df_grouped.drop(df_grouped.columns.difference(['local_authority_ons_district', 'accident_severity', 'number_of_vehicles','number_of_casualties']), 1, inplace=True)
+    df_grouped.rename(columns = {'local_authority_ons_district':'Local Authority District Code', 'accident_severity':'Accident Severity', 'number_of_vehicles':'Number of vehicles', 'number_of_casualties':'Number of casualties'}, inplace = True)
+    return df_grouped
 
-def get_data():
-    # Read data
-    file_path_accidants = './data/dft-road-casualty-statistics-accident-1979-2020.csv'
-    file_path_casualty = './data/dft-road-casualty-statistics-casualty-1979-2020.csv'
-    file_path_vehicle = './data/dft-road-casualty-statistics-vehicle-1979-2020.csv'
-    
-    df_accidents = pd.read_csv(file_path_accidants)
-    df_casualty = pd.read_csv(file_path_casualty)
-    df_vehicle = pd.read_csv(file_path_vehicle)
+def merge_df(df_1 : pd.DataFrame, df_2 : pd.DataFrame) -> pd.DataFrame:
+    df_merge = pd.merge(df_1, df_2, left_on='Local Authority District Code', right_on='CODE', how='left', indicator=True)
+    df_merge['POPULATION (2018)'].iloc[df_merge['POPULATION (2018)'] == ''] = '0'
+    df_merge['POPULATION (2018)'].iloc[df_merge['_merge'] != 'both'] = '0'
+    df_merge['POPULATION (2018)'] = df_merge['POPULATION (2018)'].astype(int)
+    df_merge.drop(['CODE', '_merge'], axis=1, inplace=True)
+    df_merge.rename(columns = {'AREA':'Local Authority District Name', 'POPULATION (2018)':'Population'}, inplace=True)
+    return df_merge
 
-    # Any further data preprocessing can go her
-    frames = [df_accidents, df_casualty, df_vehicle]
-    df = pd.concat(frames, keys=['accidents', 'casualties', 'vehicles'])
-    
+def stats_per_capita(df : pd.DataFrame, stats : list) -> None:
+    for stat in stats:
+        name = f'{stat} Per capita'
+        df[name] = df[stat] / df['Population']
+        
+def load_accident_data(file_path : str) -> pd.DataFrame:
+    df = pd.read_csv(file_path, low_memory=False)
     return df
+
+def load_population_data(file_path : str) -> pd.DataFrame:
+    df = pd.read_csv(file_path, sep=';', low_memory=False)
+    df['POPULATION (2018)'] = df['POPULATION (2018)'].str.replace('.','')
+    return df
+
+def load_geojson_data(file_path : str) -> dict:
+    with open(file_path) as f:
+        geojson = json.load(f)
+        return geojson
+    
+# create_districts_df, merge_df, stats_per_capita, load_accident_data, load_population_data, load_geojson_data
